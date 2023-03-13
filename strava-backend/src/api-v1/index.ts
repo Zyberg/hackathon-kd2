9b4b51db4e34569621a9e49f2923e0832c0cff7a
Zyberg <nikolajus.elkana@gmail.com>
2023-03-13 21:18:28 +0200

    Remove work in progress files

--- strava-backend/src/api-v1/index.ts
+++ strava-backend/src/api-v1/index.ts
@@ -3,7 +3,6 @@ import { Router } from "express";
 import users from "./users/users.route";
 import challenges from "./challenges/challenges.route";
 import achievements from "./achievements/achievements.route";
-import wip from "./wip";
 import { auth } from "../helpers/auth/auth";
 
 const router: Router = Router();
@@ -14,6 +13,5 @@ router.use(auth.jwt)
 router.use("/users", users);
 router.use("/challenges", challenges);
 router.use("/achievements", achievements);
-router.use("/test", wip);
 
 export default router;
