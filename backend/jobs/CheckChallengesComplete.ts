ba8ae01dad4f9708bf37c739ed20ef59f7c83cab
Zyberg <nikolajus.elkana@gmail.com>
2023-03-24 01:24:03 +0200

    add challenge completion job

---
+++ backend/jobs/CheckChallengesComplete.ts
@@ -0,0 +1,29 @@
+import { prisma } from "../src/boot/prisma";
+
+async function CheckChallengesComplete() {
+  console.log("Start checking the Challenges for completion...");
+
+
+  const challenges = await prisma.challenge.findMany();
+
+  for (let challenge of challenges) {
+    console.log(`Checking Challenge '${challenge.title}'`)
+    console.log(challenge.endAt)
+    if (challenge.endAt < new Date()) {
+        // TODO: award achievements, prizes etc.
+
+        await prisma.challenge.update({
+            where: {
+                id: challenge.id
+            },
+            data: {
+                isComplete: true,
+            }
+        })
+    }
+  }
+
+  console.log("Finished checking all Challenges!");
+}
+
+CheckChallengesComplete();
